4206a9c9df2a3cb0c08e986dc4593875d862e07f
Ventura <ventura.py3@gmail.com>
2025-03-13 21:47:01 -0300

    feat: Film titles now are shoed

--- GridPage.xaml.cs
+++ GridPage.xaml.cs
@@ -12,7 +12,7 @@ public partial class GridPage : ContentPage
 
     public async void OnSubmit(object sender, EventArgs e)
     {
-        string url = $"https://api.themoviedb.org/3/search/movie?query={FilmName}&api_key=89cca6eda8aa59f4040823111ab90dab&include_adult=false&language=en-US&page=1";
+        string url = $"https://api.themoviedb.org/3/search/movie?query={FilmName.Text}&api_key=89cca6eda8aa59f4040823111ab90dab&include_adult=false&language=en-US&page=1";
         var result = await FetchTMDBAPI(url);
         ShowFilms(result);
     }
@@ -29,13 +29,31 @@ public partial class GridPage : ContentPage
 
     public void ShowFilms(MovieList list)
     {
+        var row = 1;
+        var column = 0;
+
         foreach (Movie movie in list.results)
         {
-            FilmsStack.Children.Add(
-                new Label
-                {
-                    Text = movie.title
-                });
+            var label = new Label
+            {
+                Text = movie.title
+            };
+
+            Grid.SetRow(label, row);
+            Grid.SetColumn(label, column);
+            MainGrid.Children.Add(label);
+
+            // Atualiza a posição para a próxima célula
+            column++;
+            if (column >= 3) // 3 colunas
+            {
+                column = 0;
+                row++;
+            }
+
+            // Para se não houver mais linhas disponíveis
+            if (row >= 4) // 4 linhas no total
+                break;
         }
     }
 }
